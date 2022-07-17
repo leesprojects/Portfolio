417649bf95834f67567228989480b7bc0c96a816
leesprojects <leepeterstevens@gmail.com>
2022-07-17 01:49:22 +0100

    Todo ==> Fix href to images on preview build

--- main.js
+++ main.js
@@ -4,14 +4,19 @@ document.querySelector('#app').innerHTML = `
   <div>
     <h1>Lee Stevens</h1>
     <div>
-    <a href="https://github.com/leesprojects"  target="_blank" rel="noopener noreferrer">
-    <img class="logo" src="./media/images/icon - github.png"></img></a>
-    <a href="https://stackoverflow.com/users/18739922/lee-stevens"  target="_blank" rel="noopener noreferrer">
-    <img class="logo" src="./media/images/icon - stackoverflow.png"></img></a>
-    <a href="https://www.linkedin.com/in/lee-stevens-dev/" target="_blank" rel="noopener noreferrer">
-    <img class="logo" src="./media/images/icon - linkedin.png"></img></a>
-    <a href="https://leetcode.com/Zaeroso/" target="_blank" rel="noopener noreferrer">
-    <img class="logo" src="./media/images/icon - leetcode.png"></img></a>
+      <a href="https://github.com/leesprojects"  target="_blank" rel="noopener noreferrer">
+        <img class="logo" src="./media/images/icon - github.png"></img></a>
+      <a href="https://stackoverflow.com/users/18739922/lee-stevens"  target="_blank" rel="noopener noreferrer">
+        <img class="logo" src="./media/images/icon - stackoverflow.png"></img></a>
+      <a href="https://www.linkedin.com/in/lee-stevens-dev/" target="_blank" rel="noopener noreferrer">
+        <img class="logo" src="./media/images/icon - linkedin.png"></img></a>
+      <a href="https://leetcode.com/Zaeroso/" target="_blank" rel="noopener noreferrer">
+        <img class="logo" src="./media/images/icon - leetcode.png"></img></a>
+    </div>
+    <div>
+      <p>Hello</p>
+      <a href="https://github.com/leesprojects"  target="_blank" rel="noopener noreferrer">
+        <img class="logo" src="./icon - github.png"></img></a>
     </div>
   </div>
 `
